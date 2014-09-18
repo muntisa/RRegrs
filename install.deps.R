6ff9c611dd64719efcd5afe0035b858ce8ab7cb5
Egon Willighagen <egon.willighagen@gmail.com>
2014-09-18 15:29:24 +0200

    Hardcode web location, as install.packages cannot find them on the mirror

--- install.deps.R
+++ install.deps.R
@@ -1,6 +1,14 @@
 install.packages(
+  pkgs=c(
+    "http://cran.us.r-project.org/src/contrib/Matrix_1.1-4.tar.gz",
+    "http://cran.us.r-project.org/src/contrib/Rcpp_0.11.2.tar.gz"
+  ),
+  repos=NULL,
+  lib="~/R_libs"
+)
+install.packages(
   c(
-    "Rcpp", "plyr", "caret","corrplot",
+    "plyr", "caret","corrplot",
     "data.table"
   ),
   repos="http://cran.us.r-project.org/",
