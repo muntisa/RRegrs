6c0a2091e3fc9a1295613dd620b617d7439c7ded
Egon Willighagen <egon.willighagen@gmail.com>
2014-09-18 15:12:40 +0200

    Indirect dependencies

--- install.deps.R
+++ install.deps.R
@@ -1,1 +1,8 @@
-install.packages(c("caret","corrplot","data.table"),repos="http://cran.us.r-project.org/",lib="~/R_libs")
+install.packages(
+  c(
+    "plyr", "caret","corrplot",
+    "data.table"
+  ),
+  repos="http://cran.us.r-project.org/",
+  lib="~/R_libs"
+)
