78c91d11060f8fb992376c26ea470e7abf4de2ca
Egon Willighagen <egon.willighagen@gmail.com>
2014-09-18 17:15:40 +0200

    More deps

--- install.deps.R
+++ install.deps.R
@@ -1,15 +1,7 @@
 install.packages(
-  pkgs=c(
-    "http://cran.us.r-project.org/src/contrib/Matrix_1.1-4.tar.gz",
-    "http://cran.us.r-project.org/src/contrib/Rcpp_0.11.2.tar.gz"
-  ),
-  repos=NULL,
-  lib="~/R_libs"
-)
-install.packages(
   c(
-    "plyr", "caret","corrplot",
-    "data.table"
+    "lattice", "codetools", "Rcpp", "Matrix",
+    "plyr", "caret","corrplot", "data.table"
   ),
   repos="http://cran.us.r-project.org/",
   lib="~/R_libs"
