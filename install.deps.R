74c74af0184fe3bc5bff0d53025e518e47fbc789
Egon Willighagen <egon.willighagen@gmail.com>
2014-09-19 10:22:09 +0200

    One more dependency that need up to date versions

--- install.deps.R
+++ install.deps.R
@@ -1,6 +1,6 @@
 install.packages(
   c(
-    "gtools",
+    "gtools", "foreach",
     "gtable", "brglm", "profileModel", "proto",
     "colorspace", "nlme", "nloptr", "digest",
     "MASS", "munsell", "stringr",
