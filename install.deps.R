f503f64e4092e9044e5858c7d12880891d93e4b1
Carlos Fernandez-Lozano <cafernandezlo@gmail.com>
2015-10-07 17:23:39 +0200

    Update install.deps.R
    minor typpo in the install.deps.R

--- install.deps.R
+++ install.deps.R
@@ -2,11 +2,11 @@ install.packages(
   c(
     "nnet",
     "gtools", "foreach", "iterators", "car",
-    "gtable", "brglm", "profileModel", "proto","glmnet"
+    "gtable", "brglm", "profileModel", "proto","glmnet",
     "colorspace", "nlme", "nloptr", "digest",
     "MASS", "munsell", "stringr",
     "lattice", "codetools", "Rcpp", "Matrix",
-    "plyr", "caret","corrplot", "data.table","corrplot"
+    "plyr", "caret","corrplot", "data.table"
   ),
   repos="http://cran.us.r-project.org/",
   lib="~/R_libs"
